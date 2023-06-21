b4ed091efd3338793f4083eb78991952f0ca4dbb
smidaJaber <js.jeber.smida@gmail.com>
2023-06-21 23:45:19 +0100

    default start command

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,6 @@ module.exports = {
     NODE_ENV: 'test'
   },
   restoreMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.ts', 'tests'],
+  coveragePathIgnorePatterns: ['node_modules', 'api/config', 'api/app.ts', 'tests'],
   coverageReporters: ['text', 'lcov', 'clover', 'html']
 };
